6a5ea36545270e28040d6d67adce977a5e473dc0
BokunoMasayume <BokunoMasayume@163.com>
2019-03-10 18:09:38 +0800

    server can auto set binded ip

--- sock-server/file-server.cpp
+++ sock-server/file-server.cpp
@@ -8,17 +8,32 @@
 #include<fcntl.h>
 #include<pthread.h>
 
+#include<string.h>
+#include<sys/types.h>
+#include<ifaddrs.h>
+
 #define MAXBUF 1024
 
 void *tranfer_file_2clnt(void* clnt_sock);
+char* get_host_ip(char* addr);
 
 int main(){
 	int serv_sock = socket(AF_INET , SOCK_STREAM , IPPROTO_TCP);
 
 	struct sockaddr_in serv_addr;
+
+	char host_addr_ip[INET_ADDRSTRLEN];
+	if(!get_host_ip(host_addr_ip)){
+		printf("Fail to get local host ip");
+		return 0;
+	}	
+	else{
+		printf("server will bind to %s :%d\n",host_addr_ip,8777);
+	}
+
 	memset(&serv_addr ,0 ,sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_addr.sin_addr.s_addr = inet_addr(host_addr_ip);
 	serv_addr.sin_port = htons(8777);
 
 	bind(serv_sock , (struct sockaddr*)&serv_addr , sizeof(serv_addr));
@@ -68,4 +83,22 @@ void *tranfer_file_2clnt(void *clnt_sock){
 	close(fp);
 	close(sock);
 	pthread_exit(NULL);
+}
+
+char* get_host_ip(char*addr){
+	struct ifaddrs* ifAddrStruct = NULL;
+	void *tmp=NULL;
+
+	getifaddrs(&ifAddrStruct);
+
+	while(ifAddrStruct !=NULL ){
+		if(ifAddrStruct->ifa_addr->sa_family ==AF_INET && !strcmp(ifAddrStruct->ifa_name,"eth0")){
+			tmp = &((struct sockaddr_in *)ifAddrStruct->ifa_addr)->sin_addr;
+			inet_ntop(AF_INET,tmp,addr,INET_ADDRSTRLEN);
+			return addr;
+		}
+		ifAddrStruct=ifAddrStruct->ifa_next;
+	}
+
+	return NULL;
 }
